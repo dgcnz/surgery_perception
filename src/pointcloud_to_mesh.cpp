b458989b124b6f8ff88f5771d3eadabdc9ab5650
Diego Canez <canezdiego@gmail.com>
2021-11-17 21:49:36 -0500

    Updated point cloud to mesh

--- src/pointcloud_to_mesh.cpp
+++ src/pointcloud_to_mesh.cpp
@@ -1,20 +1,43 @@
+#include <lvr2/algorithm/CleanupAlgorithms.hpp>
+#include <lvr2/algorithm/ClusterAlgorithms.hpp>
+#include <lvr2/algorithm/Materializer.hpp>
+#include <lvr2/algorithm/NormalAlgorithms.hpp>
+#include <lvr2/algorithm/Texturizer.hpp>
 #include <lvr2/geometry/BaseVector.hpp>
 #include <lvr2/geometry/HalfEdgeMesh.hpp>
+#include <lvr2/geometry/Handles.hpp>
 #include <lvr2/geometry/Normal.hpp>
+#include <lvr2/io/PointBuffer.hpp>
+#include <lvr2/reconstruction/AdaptiveKSearchSurface.hpp>
+#include <lvr2/reconstruction/BilinearFastBox.hpp>
+#include <lvr2/reconstruction/FastReconstruction.hpp>
+#include <lvr2/reconstruction/HashGrid.hpp>
+#include <lvr2/reconstruction/PointsetGrid.hpp>
+#include <lvr2/reconstruction/PointsetSurface.hpp>
+#include <lvr2/util/ClusterBiMap.hpp>
 #include <memory>
-#include <mesh_conversions/conversions.h>
 #include <mesh_msgs/MeshGeometryStamped.h>
+#include <mesh_msgs_conversions/conversions.h>
 #include <ros/ros.h>
 #include <sensor_msgs/PointCloud2.h>
 #include <utility>
+#include <vector>
 
 class ReconstructionNode
 {
-    using Vec       = lvr2::BaseVector<float>;
-    using PsSurface = lvr2::PointsetSurface<Vec>;
+    using Vec                     = lvr2::BaseVector<float>;
+    using PsSurface               = lvr2::PointsetSurface<Vec>;
+    std::string const cloud_topic = "camera/depth_registered/points";
+    std::string const mesh_topic  = "lvr2/reconstruction";
 
   public:
-    ReconstructionNode(ros::NodeHandle &nh);
+    ReconstructionNode(ros::NodeHandle &nh) : nh_(nh)
+    {
+        cloud_sub_ = nh_.subscribe(
+            cloud_topic, 20, &ReconstructionNode::pointCloud2Callback, this);
+        mesh_pub_ =
+            nh_.advertise<mesh_msgs::MeshGeometryStamped>(mesh_topic, 1);
+    }
 
   private:
     ros::NodeHandle    nh_;
@@ -22,23 +45,21 @@ class ReconstructionNode
     ros::Publisher     mesh_pub_;
     ros::ServiceServer service_;
 
-    ReconstructionNode(ros::NodeHandle &nh) : nh_(nh) {}
-
     void pointCloud2Callback(sensor_msgs::PointCloud2::ConstPtr const &cloud)
     {
         mesh_msgs::MeshGeometryStamped mesh_msg;
-        generateMeshGeometry(cloud, mesh_msg);
+        createMeshGeometryMessage(*cloud, mesh_msg);
         mesh_pub_.publish(mesh_msg);
     }
 
-    bool
-    createMeshGeometryMessage(sensor_msgs::PointCloud2 const &      cloud,
-                              mesh_msgs::MeshGeometryStamped const &mesh_msg)
+    bool createMeshGeometryMessage(sensor_msgs::PointCloud2 const &cloud,
+                                   mesh_msgs::MeshGeometryStamped &mesh_msg)
     {
         lvr2::MeshBufferPtr  mesh_buffer_ptr(new lvr2::MeshBuffer);
         lvr2::PointBufferPtr point_buffer_ptr(new lvr2::PointBuffer);
 
-        if (!mesh_msgs::fromPointCloud2ToPointBuffer(cloud, *point_buffer_ptr))
+        if (!mesh_msgs_conversions::fromPointCloud2ToPointBuffer(
+                cloud, *point_buffer_ptr))
         {
             ROS_ERROR_STREAM("Could not convert point cloud from "
                              "sensor_msgs::PointCloud2 to "
@@ -47,50 +68,50 @@ class ReconstructionNode
         }
         if (!createMeshGeometry(point_buffer_ptr, mesh_buffer_ptr))
         {
-            ROS_ERROR_STREAM("Reconstruction failed!");
+            ROS_ERROR_STREAM("Reconstruction Failed!");
             return false;
         }
-
-        lvr2::HalfEdgeMesh<Vec> hem;
-        if (!createMeshGeometry(point_buffer_ptr, hem))
+        if (!mesh_msgs_conversions::fromMeshBufferToMeshGeometryMessage(
+                mesh_buffer_ptr, mesh_msg.mesh_geometry))
         {
-            ROS_ERROR_STREAM(
-                "Could not convert point cloud from lvr2::MeshBuffer to "
-                "lvr::HalfEdgeMesh");
+
+            ROS_ERROR_STREAM("Could not convert point cloud from mesh_buffer "
+                             "to Geometry Message!");
             return false;
         }
-
-        mesh_msg = mesh_msgs::toMeshGeometryStamped(
-            hem, cloud.header.frame_id, to_string(cloud.header.seq));
+        mesh_msg.header.frame_id = cloud.header.frame_id;
+        mesh_msg.header.stamp    = cloud.header.stamp;
+        mesh_msg.uuid            = std::to_string(cloud.header.seq);
         return true;
     }
 
-    std::pair<std::shared_ptr<GridBase>,
-              std::unique_ptr<FastReconstructionBase<Vec>>>
-    createGridAndReconstruction(PointsetSurfacePtr<Vec> surface)
+    std::pair<std::shared_ptr<lvr2::GridBase>,
+              std::unique_ptr<lvr2::FastReconstructionBase<Vec>>>
+    createGridAndReconstruction(lvr2::PointsetSurfacePtr<Vec> surface)
     {
         int   intersections = -1;
         float voxelsize     = 10;
 
         bool   useVoxelSize      = intersections <= 0;
-        float  resolutaion       = useVoxelSize ? voxelsize : intersections;
+        float  resolution        = useVoxelSize ? voxelsize : intersections;
         string decompositionType = "PMC";
 
-        BilinearFastBox<Vec>::m_surface = surface;
-        auto grid = std::make_shared<PointsetGrid<Vec, BilinearFastBox<Vec>>>(
+        lvr2::BilinearFastBox<Vec>::m_surface = surface;
+        auto grid                             = std::make_shared<
+            lvr2::PointsetGrid<Vec, lvr2::BilinearFastBox<Vec>>>(
             resolution,
             surface,
             surface->getBoundingBox(),
             useVoxelSize,
             false);
         grid->calcDistanceValues();
-        auto reconstruction =
-            make_unique<FastReconstruction<Vec, BilinearFastBox<Vec>>>(grid);
+        auto reconstruction = make_unique<
+            lvr2::FastReconstruction<Vec, lvr2::BilinearFastBox<Vec>>>(grid);
         return make_pair(grid, std::move(reconstruction));
     }
 
-    bool createMeshGeometry(lvr2::PointBufferPtr &   point_buffer_ptr,
-                            lvr2::HalfEdgeMesh<Vec> &mesh)
+    bool createMeshGeometry(lvr2::PointBufferPtr &point_buffer_ptr,
+                            lvr2::MeshBufferPtr & mesh_buffer)
     {
         auto surface = createSurface(point_buffer_ptr);
         if (!surface)
@@ -99,11 +120,43 @@ class ReconstructionNode
             return false;
         }
         auto [grid, reconstruction] = createGridAndReconstruction(surface);
+        lvr2::HalfEdgeMesh<Vec> mesh;
         reconstruction->getMesh(mesh);
+        int cleanContourIterations = 0;
+        lvr2::cleanContours(mesh, cleanContourIterations, 0.0001);
+        auto faceNormals = lvr2::calcFaceNormals(mesh);
+
+        float                                normal_threshold = 0.85;
+        lvr2::ClusterBiMap<lvr2::FaceHandle> clusterBiMap =
+            lvr2::planarClusterGrowing(mesh, faceNormals, normal_threshold);
+
+        // TODO
+        lvr2::ClusterPainter painter(clusterBiMap);
+        auto                 clusterColors =
+            boost::optional<lvr2::DenseClusterMap<lvr2::Rgb8Color>>(
+                painter.simpsons(mesh));
+        auto vertexColors = lvr2::calcColorFromPointCloud(mesh, surface);
+        auto vertexNormals =
+            lvr2::calcVertexNormals(mesh, faceNormals, *surface);
+        lvr2::TextureFinalizer<Vec> finalize(clusterBiMap);
+        finalize.setVertexNormals(vertexNormals);
+        lvr2::Materializer<Vec> materializer(
+            mesh, clusterBiMap, faceNormals, *surface);
+        int texel_size           = 1;
+        int tex_min_cluster_size = 100;
+        int tex_max_cluster_size = 0;
+
+        lvr2::Texturizer<Vec> texturizer(
+            texel_size, tex_min_cluster_size, tex_max_cluster_size);
+
+        lvr2::MaterializerResult<Vec> matResult =
+            materializer.generateMaterials();
+        finalize.setMaterializerResult(matResult);
+        mesh_buffer = finalize.apply(mesh);
         return true;
     }
 
-    PointsetSurfacePtr<Vec>
+    lvr2::PointsetSurfacePtr<Vec>
     createSurface(lvr2::PointBufferPtr &point_buffer_ptr)
     {
         string pcm_name         = "FLANN"; // default value
@@ -112,8 +165,8 @@ class ReconstructionNode
         int    ki               = 10;
         int    kn               = 10;
 
-        PointsetSurfacePtr<Vec> surface =
-            make_shared<AdaptiveKSearchSurface<Vec>>(
+        lvr2::PointsetSurfacePtr<Vec> surface =
+            make_shared<lvr2::AdaptiveKSearchSurface<Vec>>(
                 point_buffer_ptr, pcm_name, kn, ki, kd, plane_fit_method);
 
         surface->setKd(kd);
